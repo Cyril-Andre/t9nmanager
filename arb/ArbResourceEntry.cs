2676e0862e84a9956cedb446f51e449d6053242f
Cyril ANDRE <the.cyril.andre@gmail.com>
2022-03-27 16:38:36 +0200

    Add backend ARB structure Implement JsonConverter for serialization/deserialization

---
+++ arb/ArbResourceEntry.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace arb
+{
+    public class ArbResourceCollection
+    {
+        public string Locale { get; set; }
+        public string Context { get; set; }
+        public List<ArbResourceEntry> ArbEntries { get; set; } = new List<ArbResourceEntry>();
+
+    }
+    public class ArbResourceEntry
+    {
+        private Guid _internalId;
+        private readonly bool _isNew = true;
+        public ArbResourceEntry(Guid? id)
+        {
+            if (id.HasValue) { _internalId = id.Value; _isNew = false; }
+        }
+
+        public string Id { get; set; }
+        public string Value { get; set; }
+        public ArbReourceEntryAttribute Type { get; set; } = new ArbReourceEntryAttribute("type");
+        public ArbReourceEntryAttribute Context { get; set; } = new ArbReourceEntryAttribute("context");
+        public ArbReourceEntryAttribute Description { get; set; } = new ArbReourceEntryAttribute("description");
+        public ArbReourceEntryAttribute Source_text { get; set; } = new ArbReourceEntryAttribute("source_text");
+        public ArbReourceEntryAttribute Screen { get; set; } = new ArbReourceEntryAttribute("screen");
+
+
+    }
+
+
+    public class ArbReourceEntryAttribute
+    {
+        private string _attributeName="";
+        public ArbReourceEntryAttribute(string attributeName) { _attributeName = attributeName; }
+        public string AttributeName => _attributeName;
+        public string AttributeValue { get; set; }
+    }
+}
