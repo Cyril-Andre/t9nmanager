7c27aab2244c6a95125cbfe458264a17f66a9eb9
Cyril ANDRE <the.cyril.andre@gmail.com>
2022-04-07 15:59:28 +0200

    Add email confirmation process to the registration. Forbid login if account not activated. Handle html email templating

---
+++ t9n.api/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace t9n.api
+{
+    public class AppSettings
+    {
+        public string ConfirmationEmailUrl { get; set; }
+        public string TemplatesPath { get; set; }
+    }
+}
--- t9n.api/Controllers/UserController.cs
+++ t9n.api/Controllers/UserController.cs
@@ -4,7 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Communication;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
 using Microsoft.VisualBasic;
 using t9n.api.model;
 using t9n.api.model.extension;
@@ -18,9 +20,12 @@ namespace t9n.api.Controllers
     public class UserController : ControllerBase
     {
         private readonly t9nDbContext _context;
-        public UserController(t9nDbContext context)
+        private readonly IOptions<AppSettings> _appSettings;
+
+        public UserController(t9nDbContext context, IOptions<AppSettings> appSettings)
         {
             _context = context;
+            _appSettings = appSettings;
         }
         [HttpPost("register")]
         public IActionResult Register(UserRegistrationModel userRegistrationModel)
@@ -36,11 +41,12 @@ namespace t9n.api.Controllers
                 if (!userRegistrationModel.Validate(out var reason))
                 {
                     return BadRequest(
-                        new ApiMessage(httpStatus: 400, message: "User cannot register", moreInfo: reason));
+                        new ApiMessage(httpStatus: 403, message: "User cannot register", moreInfo: reason));
                 }
                 var dbUser = userRegistrationModel.ToDatabase();
                 _context.Users.Add(dbUser);
                 _context.SaveChanges();
+                CommunicationHelper.SendConfirmationMail(userRegistrationModel.UserEmail,$"{_appSettings.Value.ConfirmationEmailUrl}?o={dbUser.UserInternalId:D}",_appSettings.Value.TemplatesPath,"en");
                 return Ok(new ApiMessage (httpStatus: 200, message: $"User is registered with a {reason} password"));
             }
             catch (Exception ex)
@@ -54,12 +60,16 @@ namespace t9n.api.Controllers
         {
             try
             {
-                if (userLogin.ValidateCredentials(_context))
+                if (userLogin.ValidateCredentials(_context,out var reason))
                 {
                     return Ok(new ApiMessage(httpStatus: 200,message:"Login successfully"));
                 }
                 else
                 {
+                    if (!string.IsNullOrEmpty(reason)) // when password is wrong, reason remains empty
+                    {
+                        return Unauthorized(new ApiMessage(httpStatus: 403, message: "Login failed", moreInfo: reason));
+                    }
                     return BadRequest(new ApiMessage (httpStatus: 400, message: "Login failed"));
                 }
             }
@@ -67,6 +77,24 @@ namespace t9n.api.Controllers
             {
                 return StatusCode(500, new ApiMessage (httpStatus: 500, message: "Cannot login", moreInfo: $"{ex.Message}"));
             }
+        }
+
+        /// <summary>
+        /// ConfirmationEmail call comes from email client. During registration, an email has been sent to the user to validate his email.
+        /// The link contains User internal Id (guid).
+        /// This method accept a Get (rather than a Put/Post because it comes from simple email.
+        /// </summary>
+        /// <param name="o"></param>
+        /// <returns></returns>
+        [HttpGet("confirm")]
+        public IActionResult ConfirmationEmail(string o)
+        {
+            Guid reference = Guid.Parse(o);
+            var user = _context.Users.FirstOrDefault(u => u.UserInternalId == reference);
+            if (user == null) return NotFound(new ApiMessage(httpStatus: 404, message: "User unknown."));
+            user.UserEmailValidated = true;
+            _context.SaveChanges();
+            return Ok(new ApiMessage(httpStatus: 200, message: "Account activated."));
         }
     }
 }
