e1f65efde9d174d1f330c39b3325bd41349b86b9
cyril-andre <the.cyril.andre@gmail.com>
2022-05-18 11:16:38 +0200

    Separate BR/FE

---
+++ t9n_manager_dotnet_api/t9n.DAL/Migrations/20220406091016_20220406.2_AddUserPasswordHash_Salt.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace t9n.DAL.Migrations
+{
+    public partial class _202204062_AddUserPasswordHash_Salt : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserPassword",
+                table: "Users",
+                newName: "UserPasswordHash");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Salt",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Salt",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "UserPasswordHash",
+                table: "Users",
+                newName: "UserPassword");
+        }
+    }
+}
---
+++ t9n_manager_dotnet_api/t9n.DAL/t9nDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+namespace t9n.DAL
+{
+    public class t9nDbContext: DbContext
+    {
+        public t9nDbContext(DbContextOptions<t9nDbContext> options) : base(options) { }
+        public DbSet<DbUser> Users { get; set; }
+        public DbSet<DbArbResourceEntryAttribute> ArbAttributes { get; set; }
+        public DbSet<DbArbResourceEntry> ArbEntry { get; set; }
+        public DbSet<DbArbResourceEntryCollection> ArbCollection { get; set; }
+    }
+}
